44862d5ea5e6c35dc41727e162d92491af97f10f
Zabiullah-sz <71023764+Zabiullah-sz@users.noreply.github.com>
2020-09-16 11:31:22 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-def is_even_len(string: str) -> bool:
-    return False
+def is_even_len(string: Zabiullah) -> bool:
+    return len(string)
+    
 
 
-def remove_third_char(string: str) -> str:
-    return ""
+def remove_third_char(string: Zabiullah) -> str:
+    chaine = str[:2]+str[3:]
+ 	return chaine
 
 
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-    return ""
+def replace_char(string: Zabiullah, old_char: b, new_char: f) -> str:
+    old_char_index = string.find(old_char)
+    return string[:old_char_index] + new_char + string[old_char_index + 1:]
 
 
 def get_nb_char(string: str, char: str) -> int:
